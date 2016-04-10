d83591337b81d8e5409ce8db4db92d0daecf197e
Robin40 <r.a.c.f.28@gmail.com>
2016-04-09 22:28:49 -0300

    Timeline Creator

---
+++ static/timeline_creator.js
@@ -0,0 +1,16 @@
+window.userTimeline = emptyTimelineJSON;
+function timelinePreview_update() {
+	var div = document.getElementById('timelinePreview');
+	if (!div.hasChildNodes()) {
+		div.appendChild(timeline_div(userTimeline));
+		return;
+	}
+	
+	div.removeChild(div.firstChild);
+	div.appendChild(timeline_div(userTimeline));
+}
+
+function add_event(ev) {
+	userTimeline.events.push(ev);
+	timelinePreview_update();
+}
