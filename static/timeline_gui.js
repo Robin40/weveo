0e7c648023ecf3576611d0e60176ef7d72f9fabd
Robin40 <r.a.c.f.28@gmail.com>
2016-04-09 23:51:25 -0300

    Timeline Creator v2

--- static/timeline_gui.js
+++ static/timeline_gui.js
@@ -7,11 +7,15 @@ function hours_diff(bhour, ehour) {
 }
 
 function twoDigs2str(x) {
-	return int(x/10) + "" + x%10;
+	return int(x/10) + "" + int(x%10);
 }
 
 function hour2str(hour) {
-	return twoDigs2str(int(hour/60)%24) + ":" + twoDigs2str(hour%60);
+	return twoDigs2str(int(hour/60)%24) + ":" + twoDigs2str(int(hour%60));
+}
+
+function hour2durStr(hour) {
+	return int(hour/60) + "h" + twoDigs2str(int(hour%60)) + "m";
 }
 
 function contrast_color(col) {
@@ -26,14 +30,22 @@ function unique_color(i) {
 
 function new_div() {return document.createElement("div");}
 
-window.eventDetailsBgcol = "light gray";
+window.eventDetailsBgcol = "LightGray";
 function eventDetails_div(ev) {
 	var div = new_div();
 	div.style.border = "solid black";
 	div.style.background = eventDetailsBgcol;
 	div.style.display = "none";
-	div.innerHTML = "Location: " + ev.location;
+	div.innerHTML = "Duration: " + hour2durStr(ev.ehour - ev.bhour) + 
+		"<br>Location: <a href='" + ev.directions + "'>" + ev.location + "</a>";
 	return div;
+}
+
+function div_toggle(div, displayMode) {
+	if (div.style.display == "none")
+		div.style.display = displayMode;
+	else
+		div.style.display = "none";
 }
 
 window.hourWidth = 96;
@@ -42,12 +54,12 @@ window.eventFont = "Agency FB";
 window.eventBRadius = 8;
 function event_div(ev) {
 	var div = new_div();
-	div.appendChild(eventDetails_div(ev));
-	div.onclick = function() {console.log("foo");};
+	div.onclick = function() {div_toggle(this.lastChild, "block");};
 	div.style.fontFamily = eventFont;
 	div.style.textAlign = "center";
 	div.style.borderRadius = px(eventBRadius);
 	div.innerHTML = ev['name']+"<br>"+hour2str(ev.bhour)+" - "+hour2str(ev.ehour);
+	div.appendChild(eventDetails_div(ev));
 	return div;
 }
 
@@ -93,7 +105,9 @@ function timeline_div(tl) {
 	return div;
 }
 
-testTimelineJSON = JSON.parse('{"timeline": {"events": [{"ehour": 1380, "name": "Carrete Lucas", "bhour": 1230, "location": "Santa Julia"}, {"ehour": 1650, "name": "Carrete en Bella", "bhour": 1410, "location": "harvard"}]}}').timeline;
+//testTimelineJSON = JSON.parse('{"timeline": {"events": [{"ehour": 1380, "name": "Carrete Lucas", "bhour": 1230, "location": "Santa Julia"}, {"ehour": 1650, "name": "Carrete en Bella", "bhour": 1410, "location": "harvard"}]}}').timeline;
+
+testTimelineJSON = JSON.parse('{"timeline": {"events": [{"directions": "https://www.google.es/maps/dir/Genova+2016,+Region+Metropolitana/Santa+Julia+640,+Region+Metropolitana/@-33.4444689,-70.6913193,13z/", "name": "Carrete Lucas", "bhour": 1230, "ehour": 1380, "location": "Santa Julia"}, {"directions": "https://www.google.es/maps/dir/Santa+Julia+640,+Region+Metropolitana/Pio+Nono+148,+Region+Metropolitana/@-33.4444689,-70.6913193,13z/", "name": "Carrete en Bella", "bhour": 1410, "ehour": 1650, "location": "harvard"}, {"directions": "https://www.google.es/maps/dir/Pio+Nono+148,+Region+Metropolitana/Bombero+nuez+159,+Region+Metropolitana/@-33.4444689,-70.6913193,13z/", "name": "After", "bhour": 1770, "ehour": 1970, "location": "After en"}]}}').timeline;
 
 emptyTimelineJSON = {"events": []};
 
